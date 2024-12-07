b0f73ee8eedcfd6554d4822d47ad62a3ac6f18e3
FernandoYZ <fernandozavala266@gmail.com>
2024-12-06 21:55:24 -0500

    feat(prisma): crear migración inicial para la base de datos
    Se ejecutó la migración inicial de Prisma con el nombre `init`, lo que ha generado los siguientes cambios:
    - Creación de la base de datos `dev.db`.
    - Generación de las tablas `Role`, `User`, `Permission`, y `RolePermission` con sus respectivas relaciones.
    - Creación de índices únicos en los campos relevantes: `Role.name`, `User.email`, `Permission.name`, y la combinación de `RolePermission.roleId` y `RolePermission.permissionId`.
    - Generación de los archivos de migración en la carpeta `prisma/migrations`, específicamente `20241207024441_init/migration.sql`.
    
    Este cambio establece la estructura inicial de la base de datos, incluyendo las relaciones necesarias para la gestión de roles, usuarios y permisos.
    
    #Closes 0

---
+++ prisma/migrations/20241207024441_init/migration.sql
@@ -0,0 +1,48 @@
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "oauth_provider" TEXT,
+    "oauth_id" TEXT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "avatar_url" TEXT,
+    "roleId" INTEGER,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Permission" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "RolePermission" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "roleId" INTEGER NOT NULL,
+    "permissionId" INTEGER NOT NULL,
+    CONSTRAINT "RolePermission_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "RolePermission_permissionId_fkey" FOREIGN KEY ("permissionId") REFERENCES "Permission" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Permission_name_key" ON "Permission"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RolePermission_roleId_permissionId_key" ON "RolePermission"("roleId", "permissionId");
